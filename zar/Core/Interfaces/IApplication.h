8704505336383df308b4828c24b005d335775346
Yordy Leonidas <yordy.lmv.2000@gmail.com>
2022-08-11 22:32:21 -0500

    update IApp

--- zar/Core/Interfaces/IApplication.h
+++ zar/Core/Interfaces/IApplication.h
@@ -7,7 +7,7 @@ namespace zar
     public:
         inline virtual ~IApplication() = default;
         inline IApplication() = default;
-    protected:
+
         inline virtual void start() = 0;
         inline virtual void update() = 0;
         inline virtual void render() = 0;
