3be9d267a39f1d5a10b1bb820fd5db6905436e32
Yordy Leonidas <yordy.lmv.2000@gmail.com>
2022-08-14 23:32:12 -0500

    add update core

---
+++ zar/core/interfaces/IComponent.cpp
@@ -0,0 +1,10 @@
+﻿#include "C:/dev/C++/grid/build/vendor/zar/CMakeFiles/zar.dir/Debug/cmake_pch.hxx"
+#include "IComponent.h"
+
+void zar::IComponent::start()
+{
+}
+
+void zar::IComponent::update()
+{
+}
---
+++ zar/core/interfaces/IComponent.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+namespace zar
+{
+    class ZAR_API IComponent
+    {
+    public:
+        ZAR_INLINE virtual ~IComponent() = default;
+        ZAR_INLINE IComponent() = default;
+        
+        ZAR_INLINE virtual void start();
+        ZAR_INLINE virtual void update();
+    };
+}
